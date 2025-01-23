45eedd201596c4c2e3aa22f268f9493e082081b0
Jens Ameye <jens@exsited.be>
2025-01-23 13:55:56 +0100

    Add REST functionality and views

--- index.js
+++ index.js
@@ -1,71 +1,95 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const laptops = 'https://api.hubspot.com/crm/v3/objects/laptops?properties=Name&properties=Price&properties=Status';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        const resp = await axios.get(laptops, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Object Table | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error('Error fetching laptops data:', error.response?.data || error.message);
     }
-
 });
-*/
 
+app.get('/update-cobj/:id?', async (req, res) => {
+    const id = req.params.id;
+    const laptopUrl = `https://api.hubspot.com/crm/v3/objects/laptops${id ? `/${id}` : ''}?properties=Name&properties=Price&properties=Status`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json',
+    };
 
-// * Localhost
+    try {
+        let data = {};
+        if (id) {
+            const resp = await axios.get(laptopUrl, { headers });
+            data = resp.data;
+        }
+
+        res.render('updates', {
+            title: id ? `Update Custom Object Form | Integrating With HubSpot I Practicum` : `Create Custom Object Form | Integrating With HubSpot I Practicum`,
+            data: id ? data : {},
+        });
+    } catch (error) {
+        res.status(500).send('Error fetching laptop details');
+    }
+});
+
+app.post('/update-cobj', async (req, res) => {
+    const { id, name, status, price } = req.body;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json',
+    };
+
+    if (id) {
+        const updateURL = `https://api.hubspot.com/crm/v3/objects/laptops/${id}`;
+        const updateData = {
+            properties: {
+                name: name,
+                status: status,
+                price: price,
+            },
+        };
+
+        try {
+            const response = await axios.patch(updateURL, updateData, { headers });
+            res.redirect('/');
+        } catch (error) {
+            res.status(500).send('Error updating laptop');
+        }
+    } else {
+        const createURL = `https://api.hubspot.com/crm/v3/objects/laptops`;
+        const createData = {
+            properties: {
+                name: name,
+                status: status,
+                price: price,
+            },
+        };
+
+        try {
+            const response = await axios.post(createURL, createData, { headers });
+            res.redirect('/');
+        } catch (error) {
+            res.status(500).send('Error creating laptop');
+        }
+    }
+});
+
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
